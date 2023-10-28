bc4efe96a967cc1fd84a61f0b9f5c72b8b9f7639
Otilija04 <145570265+Otilija04@users.noreply.github.com>
2023-10-28 19:41:58 +0300

    atskyriau funkcijas, įdėjau if else, kad patikrintų, ar yra jau sukurtas toks failas

--- main.cpp
+++ main.cpp
@@ -1,69 +1,73 @@
-#include "Library.h"
+#include "lib.h"
 #include <chrono>
+#include <fstream>
+
+bool fileExists(const std::string& fileName) {
+    std::ifstream file(fileName.c_str());
+    return file.good();
+}
+
+void generateAndSaveStudents(int numStudents, const std::string& fileName, double& fileCreateDuration) {
+    if (!fileExists(fileName)) {
+        std::vector<Student> students;
+
+        auto fileCreateStart = std::chrono::high_resolution_clock::now();
+        generateStudents(numStudents, students);
+        saveStudentsToFile(students, fileName);
+        auto fileCreateEnd = std::chrono::high_resolution_clock::now();
+
+        fileCreateDuration = std::chrono::duration<double>(fileCreateEnd - fileCreateStart).count();
+        std::cout << "studentu kurimas uztruko " << fileCreateDuration << " sec." << std::endl;
+    } else {
+        std::cout << "File " << fileName << " already exists. Skipping generation." << std::endl;
+        fileCreateDuration = 0.0; 
+    }
+}
+
+void readAndProcessStudents(const std::string& fileName, double& fileReadDuration, double& fileSortDuration) {
+    auto fileReadStart = std::chrono::high_resolution_clock::now();
+    std::vector<Student> students = readStudentData(fileName);
+
+    auto fileSortStart = std::chrono::high_resolution_clock::now();
+
+    for (Student& student : students) {
+        int sum = 0;
+        for (int grade : student.homeworkGrades) {
+            sum += grade;
+        }
+        student.avg = static_cast<double>(sum) / student.homeworkGrades.size();
+    }
+
+    std::vector<Student> badStudents, goodStudents;
+    separateStudentsByAverage(students, badStudents, goodStudents);
+
+    auto fileSortEnd = std::chrono::high_resolution_clock::now();
+    fileSortDuration = std::chrono::duration<double>(fileSortEnd - fileSortStart).count();
+
+    std::cout << "studentu rusiavimas i dvi grupes uztruko " << fileSortDuration << " sec." << std::endl;
+
+    auto fileReadEnd = std::chrono::high_resolution_clock::now();
+    fileReadDuration = std::chrono::duration<double>(fileReadEnd - fileReadStart).count();
+    std::cout << "studentu nuskaitymas uztruko " << fileReadDuration << " sec." << std::endl;
+
+    saveStudentsToFile(badStudents, "bad" + fileName);
+    saveStudentsToFile(goodStudents, "good" + fileName);
+}
 
 int main() {
-	int numStudents[] = { 1000, 10000, 100000, 1000000, 10000000 };
-	vector<Student> students;
+    int numStudents[] = {1000, 10000, 100000, 1000000, 10000000};
 
-	for (int i = 0; i < 5; ++i) {
-		cout << numStudents[i] << " studentu spartos analize:" << "\n";
+    for (int i = 0; i < 5; ++i) {
+        std::string fileName = "kursiokai" + std::to_string(numStudents[i]) + ".txt";
+        std::cout << numStudents[i] << " studentu spartos analize:" << "\n";
 
-		auto fileCreateStart = chrono::high_resolution_clock::now();
+        double fileCreateDuration, fileReadDuration, fileSortDuration;
+        generateAndSaveStudents(numStudents[i], fileName, fileCreateDuration);
+        readAndProcessStudents(fileName, fileReadDuration, fileSortDuration);
 
-		generateStudents(numStudents[i], students);
-		saveStudentsToFile(students, "kursiokai" + to_string(numStudents[i]) + ".txt");
+        std::cout << "studentu idejimas i du naujus failus uztruko " << fileCreateDuration + fileSortDuration + fileReadDuration << " sec." << std::endl;
+        std::cout << "\n\n";
+    }
 
-		auto fileCreateEnd = chrono::high_resolution_clock::now();
-		chrono::duration<double> fileCreateDuration = fileCreateEnd - fileCreateStart;
-		cout << "studentu kurimas uztruko " << fileCreateDuration.count() << " sec." << endl;
-
-
-
-
-		auto fileReadStart = chrono::high_resolution_clock::now();
-
-		readStudentData("kursiokai" + to_string(numStudents[i]) + ".txt");
-
-		auto fileReadEnd = chrono::high_resolution_clock::now();
-		chrono::duration<double> fileReadDuration = fileReadEnd - fileReadStart;
-		cout << "studentu nuskaitymas uztruko " << fileReadDuration.count() << " sec." << endl;
-
-
-
-		auto fileSortStart = chrono::high_resolution_clock::now();
-
-		for (Student& student : students) {
-			int sum = 0;
-			for (int grade : student.homeworkGrades) {
-				sum += grade;
-			}
-			student.avg = static_cast<double>(sum) / student.homeworkGrades.size();
-		}
-
-		vector<Student> badStudents, goodStudents;
-		separateStudentsByAverage(students, badStudents, goodStudents);
-
-		auto fileSortEnd = chrono::high_resolution_clock::now();
-		chrono::duration<double> fileSortDuration = fileSortEnd - fileSortStart;
-		cout << "studentu rusiavimas i dvi grupes uztruko " << fileSortDuration.count() << " sec." << endl;
-
-
-
-
-		auto fileSaveStart = chrono::high_resolution_clock::now();
-
-		saveStudentsToFile(badStudents, "badstudents" + std::to_string(numStudents[i]) + ".txt");
-		saveStudentsToFile(goodStudents, "goodstudents" + std::to_string(numStudents[i]) + ".txt");
-
-		auto fileSaveEnd = chrono::high_resolution_clock::now();
-		chrono::duration<double> fileSaveDuration = fileSaveEnd - fileSaveStart;
-		cout << "studentu idejimas i du naujus failus uztruko " << fileSaveDuration.count() << " sec." << endl << "\n\n";
-
-
-
-
-		students.clear();
-	}
-
-	return 0;
+    return 0;
 }
