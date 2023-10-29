67a74255a550815b208d29c7820fa868d1f45855
Otilija04 <145570265+Otilija04@users.noreply.github.com>
2023-10-29 08:36:01 +0200

    Create Library.cpp

---
+++ Library.cpp
@@ -0,0 +1,75 @@
+#include "Library.h"
+
+std::list<Student> readStudentData(const std::string& filename) {
+    std::ifstream inputFile(filename);
+    if (!inputFile) {
+        std::cerr << "Error: Unable to open the file: " << filename << std::endl;
+        exit(1);
+    }
+
+    std::list<Student> students;
+    std::string header;
+    getline(inputFile, header);
+
+    std::string firstName, lastName;
+    int exam;
+    std::list<int> homeworkGrades;
+
+    while (inputFile >> firstName >> lastName) {
+        homeworkGrades.clear();
+        int grade;
+        for (int i = 0; i < 7; ++i) {
+            inputFile >> grade;
+            homeworkGrades.push_back(grade);
+        }
+        inputFile >> exam;
+        students.push_back({ firstName, lastName, homeworkGrades, exam });
+    }
+
+    inputFile.close();
+    return students;
+}
+
+int generateRandomNumber(int n, int n1) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> dis(n, n1);
+    return dis(gen);
+}
+
+void generateStudents(int numStudents, std::list<Student>& students) {
+    for (int i = 0; i < numStudents; ++i) {
+        students.push_back({ "Vardas" + std::to_string(i), "Pavarde" + std::to_string(i), {}, generateRandomNumber(1, 10) });
+        for (int j = 0; j < 7; ++j) {
+            students.back().homeworkGrades.push_back(generateRandomNumber(1, 10));
+        }
+    }
+}
+
+void saveStudentsToFile(const std::list<Student>& students, const std::string& fileName) {
+    std::ofstream file(fileName);
+    file << std::setw(10) << "Vardas" << std::setw(10) << "Pavarde";
+    for (int j = 1; j <= 7; ++j) {
+        file << std::setw(10) << "ND" + std::to_string(j);
+    }
+    file << std::setw(10) << "Egz." << std::endl;
+
+    for (const Student& student : students) {
+        file << std::setw(10) << student.firstName << std::setw(10) << student.lastName;
+        for (int grade : student.homeworkGrades) {
+            file << std::setw(10) << grade;
+        }
+        file << std::setw(10) << student.exam << std::endl;
+    }
+}
+
+void separateStudentsByAverage(const std::list<Student>& students, std::list<Student>& badStudents, std::list<Student>& goodStudents) {
+    for (const Student& student : students) {
+        if (student.avg < 5) {
+            badStudents.push_back(student);
+        }
+        else {
+            goodStudents.push_back(student);
+        }
+    }
+}
