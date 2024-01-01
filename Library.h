548dd7e1c035ec2e1306be6de4d7f44cb5bed98c
Otilija04 <145570265+Otilija04@users.noreply.github.com>
2024-01-01 17:44:05 +0200

    Update Library.h

--- Library.h
+++ Library.h
@@ -11,14 +11,34 @@
 #include <filesystem>
 #include <chrono>
 
-class Student {
+// isankstine deklaracija person klases
+class Person;
+
+// Abstrakti Person klase
+class Person {
 public:
     std::string firstName;
     std::string lastName;
+
+    // konstruktorius
+    Person() {}
+    Person(const std::string& first, const std::string& last) : firstName(first), lastName(last) {}
+
+    
+    virtual ~Person() {}
+};
+
+class Student : public Person {
+public:
     std::list<int> homeworkGrades;
     int exam;
     double avg;
     double med;
+
+    // konstruktoriai
+    Student() : exam(0), avg(0.0), med(0.0) {}
+    Student(const std::string& first, const std::string& last, const std::list<int>& grades, int e, double a, double m)
+        : Person(first, last), homeworkGrades(grades), exam(e), avg(a), med(m) {}
 };
 
 std::list<Student> readStudentData(const std::string& filename);
@@ -31,7 +51,6 @@ void saveStudentsToFile(const std::list<Student>& students, const std::string& f
 
 void saveStudentsToFile2(const std::list<Student>& students, const std::string& fileName);
 
-
 void separateStudentsByAverage(const std::list<Student>& students, std::list<Student>& badStudents, std::list<Student>& goodStudents);
 
-#endif 
+#endif
